be1428d62b3e23b7439ad32334626592558aaaed
Tamara Torrecillas <tamaratgz@gmail.com>
2022-05-17 17:03:04 +0200

    Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
+
+# Prefix        Verb   URI Pattern                  Controller#Action
+# articles      GET    /articles(.:format)          articles#index
+#               POST   /articles(.:format)          articles#create
+# new_article   GET    /articles/new(.:format)      articles#new
+# edit_article  GET    /articles/:id/edit(.:format) articles#edit
+# article       GET    /articles/:id(.:format)      articles#show
+#               PATCH  /articles/:id(.:format)      articles#update
+#               PUT    /articles/:id(.:format)      articles#update
+#               DELETE /articles/:id(.:format)      articles#destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating fake posts...'
+10.times do
+  new_article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph.to_s
+  )
+  new_article.save
+  puts 'Finished!'
+end
